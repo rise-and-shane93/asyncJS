0ced8a8dad1e59e6e34fa45b9acb7400fa025231
rise_and_shane93 <cdtharper@gmail.com>
2019-08-16 08:05:58 -0400

    lesson on promises

--- 2-async-programming-and-callback-functions/start/js/promises.js
+++ 2-async-programming-and-callback-functions/start/js/promises.js
@@ -3,24 +3,32 @@ const wikiUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary/';
 const peopleList = document.getElementById('people');
 const btn = document.querySelector('button');
 
-function getJSON(url, callback) {
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', url);
-  xhr.onload = () => {
-    if(xhr.status === 200) {
-      let data = JSON.parse(xhr.responseText);
-      return callback(data);
-    }
-  };
-  xhr.send();
+// Make an AJAX request
+function getJSON(url) {
+  return new Promise( (resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.onload = () => {
+      if(xhr.status === 200) {
+        let data = JSON.parse(xhr.responseText);
+        resolve(data);
+      } else {
+        reject( Error(xhr.statusText) );
+      }
+    };
+    xhr.onerror = () => reject( Error('Network error'));
+    xhr.send();
+  });
 }
 
 function getProfiles(json) {
-  json.people.map( person => {
-    getJSON(wikiUrl + person.name, generateHTML);      
-  }); 
+  const profiles = json.people.map( person => {
+    return getJSON(wikiUrl + person.name);
+  });
+  return profiles;
 }
 
+// Generate the markup for each profile
 function generateHTML(data) {
   const section = document.createElement('section');
   peopleList.appendChild(section);
@@ -32,7 +40,10 @@ function generateHTML(data) {
   `;
 }
 
-btn.addEventListener('click', (event) => {
-  getJSON(astrosUrl, getProfiles);
-  event.target.remove();
+btn.addEventListener('click', (e) => {
+  getJSON(astrosUrl)
+    .then(getProfiles)
+    .then( data => console.log(data))
+    .catch( err => console.log(err));
+  e.target.remove();
 });
